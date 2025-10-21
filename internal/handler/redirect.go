aac2e2faa3555d2e95ddf753047892f5a4befebd
denozordec <shatskiy.blog@gmail.com>
2025-10-21 15:31:36 +0700

    handler: add helper to render pretty subscriptions list after creation

---
+++ internal/handler/redirect.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// afterSubscriptionCreated redirects user to pretty subscriptions list
+func (h Handler) afterSubscriptionCreated(ctx context.Context, b *bot.Bot, chatID int64, messageID int) {
+	// Reuse MySubscriptionsCallbackHandler rendering with a synthetic callback update
+	upd := &models.Update{CallbackQuery: &models.CallbackQuery{From: &models.User{ID: chatID}, Message: &models.Message{Message: &models.Message{Chat: &models.Chat{ID: chatID}, ID: messageID}}, Data: CallbackMySubscriptions}}
+	h.MySubscriptionsCallbackHandler(ctx, b, upd)
+}
