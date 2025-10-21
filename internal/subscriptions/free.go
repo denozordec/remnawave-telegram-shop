a1c162487d7c3d1b5ba3cd9510ea6e66b9f16e4e
denozordec <shatskiy.blog@gmail.com>
2025-10-21 14:10:23 +0700

    subscriptions/free: remove unused bot/models/slog imports

--- internal/subscriptions/free.go
+++ internal/subscriptions/free.go
@@ -3,26 +3,22 @@ package subscriptions
 import (
 	"context"
 	"fmt"
-	"github.com/go-telegram/bot"
-	"github.com/go-telegram/bot/models"
-	"log/slog"
 	"remnawave-tg-shop-bot/internal/config"
 	"remnawave-tg-shop-bot/internal/database"
 	"remnawave-tg-shop-bot/internal/remnawave"
 )
 
+type Translator interface{ GetText(lang, key string) string }
+
 type Service struct {
 	SubsRepo    *database.SubscriptionRepository
 	Customers   *database.CustomerRepository
 	RW          *remnawave.Client
-	Bot         *bot.Bot
-	Translate   interface{ GetText(lang, key string) string }
+	Translate   Translator
 }
 
 func (s *Service) ActivateFree(ctx context.Context, customerTelegramID int64) (string, error) {
-	if config.TrialDays() == 0 {
-		return "", nil
-	}
+	if config.TrialDays() == 0 { return "", nil }
 	customer, err := s.Customers.FindByTelegramId(ctx, customerTelegramID)
 	if err != nil { return "", err }
 	if customer == nil { return "", fmt.Errorf("customer %d not found", customerTelegramID) }
@@ -34,14 +30,7 @@ func (s *Service) ActivateFree(ctx context.Context, customerTelegramID int64) (s
 	user, err := s.RW.CreateUserForSubscription(ctx, customer.ID, customer.TelegramID, config.TrialTrafficLimit(), config.TrialDays(), seq)
 	if err != nil { return "", err }
 
-	sub := &database.Subscription{
-		CustomerID:       customer.ID,
-		SubscriptionLink: user.SubscriptionUrl,
-		ExpireAt:         user.ExpireAt,
-		IsActive:         true,
-		Name:             fmt.Sprintf("%s #%d", s.Translate.GetText(customer.Language, "subscription_name"), seq),
-		Description:      s.Translate.GetText(customer.Language, "trial_subscription_description"),
-	}
+	sub := &database.Subscription{ CustomerID: customer.ID, SubscriptionLink: user.SubscriptionUrl, ExpireAt: user.ExpireAt, IsActive: true, Name: fmt.Sprintf("%s #%d", s.Translate.GetText(customer.Language, "subscription_name"), seq), Description: s.Translate.GetText(customer.Language, "trial_subscription_description") }
 	if _, err := s.SubsRepo.CreateSubscription(ctx, sub); err != nil { return "", err }
 	return user.SubscriptionUrl, nil
 }
