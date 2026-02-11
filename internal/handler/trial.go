ae271c1ca867ef311aa74db94589927c767efcb1
denozordec <shatskiy.blog@gmail.com>
2026-02-11 15:06:26 +0700

    feat: add Connect and Telegram buttons to trial activation response
    After successful trial subscription activation, now shows:
    - Подключиться (Connect) button - with MiniApp or callback
    - Оживить Telegram button - if TG_PROXY_LINK is set
    - Мои подписки (My Subscriptions) button
    - В главное меню (Back to Main Menu) button

--- internal/handler/trial.go
+++ internal/handler/trial.go
@@ -25,6 +25,42 @@ func (h Handler) TrialCallbackHandler(ctx context.Context, b *bot.Bot, update *m
 	// сразу рендерим красивую таблицу
 	h.afterSubscriptionCreated(ctx, b, callback.Chat.ID, callback.ID)
 	// если что-то пойдёт не так, покажем запасной текст
-	_, _ = b.EditMessageText(ctx, &bot.EditMessageTextParams{ChatID: callback.Chat.ID, MessageID: callback.ID, Text: h.translation.GetText(langCode, "trial_activated"), ParseMode: models.ParseModeHTML, ReplyMarkup: models.InlineKeyboardMarkup{InlineKeyboard: h.createConnectKeyboard(langCode)}})
+	_, _ = b.EditMessageText(ctx, &bot.EditMessageTextParams{ChatID: callback.Chat.ID, MessageID: callback.ID, Text: h.translation.GetText(langCode, "trial_activated"), ParseMode: models.ParseModeHTML, ReplyMarkup: models.InlineKeyboardMarkup{InlineKeyboard: h.createTrialSuccessKeyboard(langCode)}})
 }
 
+// createTrialSuccessKeyboard returns keyboard with Connect and Telegram buttons after trial activation
+func (h Handler) createTrialSuccessKeyboard(lang string) [][]models.InlineKeyboardButton {
+	var keyboard [][]models.InlineKeyboardButton
+
+	// Кнопка "Подключиться"
+	if config.GetMiniAppURL() != "" {
+		keyboard = append(keyboard, []models.InlineKeyboardButton{
+			{Text: h.translation.GetText(lang, "connect_button"), WebApp: &models.WebAppInfo{
+				URL: config.GetMiniAppURL(),
+			}},
+		})
+	} else {
+		keyboard = append(keyboard, []models.InlineKeyboardButton{
+			{Text: h.translation.GetText(lang, "connect_button"), CallbackData: CallbackConnect},
+		})
+	}
+
+	// Кнопка "Оживить Telegram" если TG_PROXY_LINK задан
+	if config.TgProxyLink() != "" {
+		keyboard = append(keyboard, []models.InlineKeyboardButton{
+			{Text: h.translation.GetText(lang, "tg_proxy_button"), URL: config.TgProxyLink()},
+		})
+	}
+
+	// Кнопка "Мои подписки"
+	keyboard = append(keyboard, []models.InlineKeyboardButton{
+		{Text: h.translation.GetText(lang, "my_subscriptions_button"), CallbackData: CallbackMySubscriptions},
+	})
+
+	// Кнопка "В главное меню"
+	keyboard = append(keyboard, []models.InlineKeyboardButton{
+		{Text: h.translation.GetText(lang, "back_button"), CallbackData: CallbackStart},
+	})
+
+	return keyboard
+}
